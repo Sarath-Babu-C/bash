ab1ce35abeaa656ccf0af776b065b154021fec56
xman <123232058+babuthehacker@users.noreply.github.com>
2023-01-25 10:01:23 +0530

    Create ipchanger.sh

---
+++ ipchanger.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set the interface (e.g. eth0, wlan0)
+interface=eth0
+
+# Set the new IP address
+new_ip=192.168.1.100
+
+# Set the netmask
+netmask=255.255.255.0
+
+# Set the gateway
+gateway=192.168.1.1
+
+# Set the DNS server
+dns=8.8.8.8
+
+# Change the IP address
+sudo ifconfig $interface $new_ip netmask $netmask
+
+# Change the default gateway
+sudo route add default gw $gateway $interface
+
+# Change the DNS server
+echo "nameserver $dns" | sudo tee /etc/resolv.conf > /dev/null
